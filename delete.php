0445bdb693a03ef1e5e3302ef07a0e68afd578d1
StefanMrmov <stefanmrmov@yahoo.com>
2024-11-15 18:20:29 +0100

    fai3

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id"])){
     $id = intval($_POST["id"]);
     $db = new SQLite3(__DIR__ . "/db.sqlite");
-    $stmt = $db->prepare("DELETE FROM expenses WHERE id = :id I");
+    $stmt = $db->prepare("DELETE FROM expenses WHERE id = :id");
     $stmt->bindValue(":id", $id, SQLITE3_INTEGER);
     $stmt->execute();
     $db->close();
